d876d3de39b2b9012cff29785f60ebf200192a57
Miguel Jimenez <denkatalabeno@gmail.com>
2019-09-16 18:42:47 +0800

    started tests for entries controller

--- spec/entries_controller_spec.rb
+++ spec/entries_controller_spec.rb
@@ -10,4 +10,27 @@ describe EntriesController do
       expect { EntriesController.new("parking_lot")}.not_to raise_error
     end
   end
+
+  context "receives a valid entry request" do
+    it 'should return a success message' do
+     lot = double("parking lot")
+     request = double("request")
+     allow(lot).to receive(:space_available?) { true }
+     allow(lot).to receive(:holds?).with(request) { true }
+     allow(lot).to receive(:store).with(request)
+     entries = EntriesController.new(lot)
+     entries.process(request)
+     expect(lot).to have_received(:store).with(request)
+    end
+  end
+
+  context "receives an invalid entry request - duplicated plate" do
+    it 'should return an error message' do
+    end
+  end
+
+  context "receives an invalid entry request - plate not found" do
+    it 'should return an error message' do
+    end
+  end
 end
